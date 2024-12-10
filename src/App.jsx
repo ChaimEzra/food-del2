05832aff3e42c5cd79acb17f5f2abe44e789699a
ChaimEzra <chimas1357@gmail.com>
2024-12-10 20:43:07 +0200

    chaing by viktor

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Nevbar from "./components/Nevbar/Nevbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
-    <div className="app">
-      <Nevbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Nevbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
